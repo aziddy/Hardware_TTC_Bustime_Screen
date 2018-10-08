25e4f426ceaa9297fe4577865f39efe1718507a6
Alexander Zidros <alexander.zidros@gmail.com>
2018-10-07 23:12:08 -0400

    we gucci

--- pi_version/main.py
+++ pi_version/main.py
@@ -11,56 +11,77 @@ screen = pygame.display.set_mode((800, 480))
 pygame.font.init()
 done = False
 
-
-r = requests.get('http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=ttc&r=89&s=5847')
-
-parsed_json = json.loads(r.content)
-
-TTCdata = []
-
-#print(parsed_json["predictions"]["direction"]["title"])
-
-
+countdown_minus = 0
 
 tempArray = []
+TTCdata = []
 
-for g in range(len(parsed_json["predictions"]["direction"]["prediction"])):
-	
-	tempArray.append([int(parsed_json["predictions"]["direction"]["prediction"][g]["minutes"]), (int(parsed_json["predictions"]["direction"]["prediction"][g]["seconds"]) - int(parsed_json["predictions"]["direction"]["prediction"][g]["minutes"])*60)])
 
-TTCdata.append([parsed_json["predictions"]["direction"]["title"],tempArray])
 
 print(TTCdata)
 
-
-txt = "aaaa"
-
-def client_side_countdown():
-	print("ayyy")
-	
 def async_ttc(q):
 	print("async")
 	r = requests.get('http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=ttc&r=89&s=5847')
 
+	countdown_minus = math.floor(time.clock())
+
 	parsed_json = json.loads(r.content)
-	TTCdata = []
 
 	tempArray = []
+	TTCdata = []
 
 	for g in range(len(parsed_json["predictions"]["direction"]["prediction"])):
 		
-		tempArray.append([int(parsed_json["predictions"]["direction"]["prediction"][g]["minutes"]), (int(parsed_json["predictions"]["direction"]["prediction"][g]["seconds"]) - int(parsed_json["predictions"]["direction"]["prediction"][g]["minutes"])*60)])
+		total_seconds = int(parsed_json["predictions"]["direction"]["prediction"][g]["seconds"])
+		
+		tempArray.append(total_seconds)
 
-	TTCdata.append([parsed_json["predictions"]["direction"]["title"],tempArray])
+	TTCdata.append(["89 South", tempArray])
+	
+	
+	
+	r = requests.get('http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=ttc&r=41&s=5847')
+	parsed_json = json.loads(r.content)
+	tempArray = []
+	
+	for g in range(len(parsed_json["predictions"]["direction"]["prediction"])):
+		
+		total_seconds = int(parsed_json["predictions"]["direction"]["prediction"][g]["seconds"])
+		
+		tempArray.append(total_seconds)
+
+	TTCdata.append(["41 South", tempArray])
+	
+	
 	q.put(TTCdata)
+	q.put(countdown_minus)
 
 
 q = queue.Queue()
 
-myfont = pygame.font.SysFont('Impact', 30)
+myfont = pygame.font.SysFont('arial', 30)
+
+
+
+firstValFont = pygame.font.SysFont('arial', 40)
+firstValFont.set_bold(True)
+firstValFont.set_italic(True)
+
+
+busfont = pygame.font.SysFont('arial', 30)
+busfont.set_bold(True)
+busfont.set_underline(True)
 
 local_countdown_number = 0;
 
+t = threading.Thread(target=async_ttc, args = (q,))
+t.daemon = True
+t.start()
+
+def local_countdown():
+	val = math.floor(time.clock() - countdown_minus)
+	return val
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -70,26 +91,52 @@ while not done:
 			t.daemon = True
 			t.start()
 			print("a press")
-			
-			
+		if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+			done = True
 	
-	screen.fill((0,0,0)) 
+	if pygame.mouse.get_pressed()[0]:
+		print ("You have opened a chest!")
+		t = threading.Thread(target=async_ttc, args = (q,))
+		t.daemon = True
+		t.start()
 	
-	print(math.floor(time.clock()))
+	if local_countdown() > 59:
+		t = threading.Thread(target=async_ttc, args = (q,))
+		t.daemon = True
+		t.start()
+	
+	screen.fill((250,250,250)) 
+	
+	print(math.floor(local_countdown()))
 	local_countdown_number = math.floor(time.clock())
 	
 	if not q.empty():
 		TTCdata = q.get()
+		countdown_minus = q.get()
     
 	for t in range(len(TTCdata)):
+		y = 40+(t*140)
+		pygame.draw.rect(screen, (240, 240, 240), pygame.Rect(29, y-11, 743, 123))
+		pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(30, y-10, 740, 120))
+		screen.blit(busfont.render(TTCdata[t][0], False, (0, 0, 0)),(50,y))
+		first = True;
+		
 		for k in range(len(TTCdata[t][1])):
-			screen.blit(myfont.render(str(TTCdata[t][1][k][0])+":"+str(TTCdata[t][1][k][1]), False, (255, 0, 0)),(100+(70*k),100))
-    
-	#screen.blit(myfont.render('ello', False, (255, 0, 0)),(100,100))
-	pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
-	screen.blit(myfont.render(txt, False, (255, 0, 0)),(170,180))
+			
+			total_seconds = TTCdata[t][1][k] - local_countdown()
+			
+			timer_minutes = math.floor(total_seconds/60)
+			timer_seconds = (total_seconds - timer_minutes*60)
+			
+			if first:
+				screen.blit(firstValFont.render(str(timer_minutes)+":"+str(timer_seconds), False, (80, 80, 80)),(100,y+50))
+				first = False
+			else:
+				screen.blit(myfont.render(str(timer_minutes)+":"+str(timer_seconds), False, (80, 80, 80)),(200+(110*k),y+50))
 	
 	pygame.display.update()
+
+
 
 	
 	
---
+++ pi_version/test.py
@@ -0,0 +1,9 @@
+import pygame
+import requests
+import json
+import math
+import threading
+import queue
+import time
+
+#https://www.google.ca/search?q=weathr&oq=weathr&aqs=chrome..69i57j0l5.2507j1j7&sourceid=chrome&ie=UTF-8
